8984c0711a46a0f35e51f0f90f805336db2fd48a
76luc <santamaria.luc76@gmail.com>
2022-08-13 12:26:32 -0300

    polishing details

--- src/components/Pokemons/PokemonsCard.jsx
+++ src/components/Pokemons/PokemonsCard.jsx
@@ -3,11 +3,18 @@ import { CardPokemonStyled } from './pokemons.styles';
 const PokemonsCard = ({ pokemon }) => {
   return (
     <CardPokemonStyled id={pokemon.id}>
-      <div className='flip-card-front'></div>
+      <div className='flip-card-front'>
+        <div className='inner'>
+          <h3>{pokemon?.name}</h3>
+        </div>
+      </div>
       <div className='flip-card-back'>
         <div className='inner'>
           <h3>{pokemon?.name}</h3>
-          <p>{`Altura: ${pokemon?.height}M --- Peso: ${pokemon?.weight / 10} kg`}</p>
+          <p>{`Altura: ${pokemon?.height / 10}M --- Peso: ${pokemon?.weight / 10} kg`}</p>
+          <p>{`Tipo: ${pokemon?.types.map((type) => type.type.name)}`}</p>
+          <p>{`Habilidades: ${pokemon?.abilities.map((ability) => ability.ability.name)}`}</p>
+          <p>{`Espepecie: ${pokemon?.species.name}`}</p>
         </div>
       </div>
     </CardPokemonStyled>
--- src/components/Pokemons/pokemons.styles.js
+++ src/components/Pokemons/pokemons.styles.js
@@ -10,10 +10,10 @@ export const ListContainer = styled.div`
   margin: 100px 30px 20px 20px;
   height: 80%;
   display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-  grid-gap: 30px;
+  grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+  grid-gap: 20px;
   overflow-y: Auto;
-  padding: 20px;
+  padding: 80px;
   background-color: #eee;
   border-radius: 8px;
   box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
@@ -55,14 +55,11 @@ export const CardPokemonStyled = styled.div`
 
   .flip-card-front,
   .flip-card-back {
-    width: 300px;
-    min-height: 300px;
+    width: 350px;
+    min-height: 350px;
     height: auto;
     border-radius: 10px;
     box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.9);
-    background-color: #2980b999;
-    background-image: ${({ id }) =>
-      `url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png)`};
     background-size: cover;
     background-position: center;
     text-align: center;
@@ -72,6 +69,9 @@ export const CardPokemonStyled = styled.div`
   }
 
   .flip-card-front {
+    background-color: #e67e2255;
+    background-image: ${({ id }) =>
+      `url(https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${id}.png)`};
     transform: rotateY(0deg);
   }
 
@@ -79,6 +79,7 @@ export const CardPokemonStyled = styled.div`
     position: absolute;
     top: 0;
     left: 0;
+    background-color: #fff;
     transform: rotateY(180deg);
   }
 
@@ -91,36 +92,23 @@ export const CardPokemonStyled = styled.div`
     position: absolute;
     top: 0;
     left: 0;
-    opacity: 0.6;
-  }
-
-  .flip-card-front .inner,
-  .flip-card-back .inner {
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    padding: 40px;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%) translateZ(60px) scale(0.94);
-    z-index: 2;
+    //opacity: 0.6;
   }
 
   .inner h3 {
     margin-bottom: 8px;
-    color: #ffffff;
-    font-size: 30px;
+    color: #000;
+    letter-spacing: 2px;
+    text-shadow: 2px 7px 5px rgba(0, 0, 0, 0.3), 0px -4px 10px rgba(255, 255, 255, 0.3);
+    font-size: 2rem;
     font-weight: 700;
     line-height: 40px;
     text-transform: capitalize;
   }
 
   .inner p {
-    color: #ffffff;
-    font-size: 16px;
+    color: #000;
+    font-size: 1.5rem;
     line-height: 28px;
   }
 
